69f5a127d83c81ea2dff2c03d9ec55f3b4861e17
Glitch (weather-forecast-chrome-extension) <none>
2019-08-25 10:19:21 +0000

    🌝🌽 Updated with Glitch

--- script.js
+++ script.js
@@ -1,96 +1,105 @@
-const key = "54b0c10571a1c21955309ef23d191ea5";
+window.onload = function(){
+    const key = "54b0c10571a1c21955309ef23d191ea5";
 
-let weather = {
-  name : null,
-  temperature : null,
-  humidity : null,
-  description : null,
-  iconId : null,
-  setWeather : function(data){
-    weather.name = data.name;
-    weather.temperature = Math.floor(data.main.temp - 273.15);
-    weather.humidity = data.main.humidity;
-    weather.description = "";
-    for( let i = 0; i < data.weather.length; i++){
-      if(i == 0) weather.description += data.weather[i].description + " ";
-      else weather.description += "with " + data.weather[i].description + " ";
-    }
-    weather.iconId = data.weather[0].icon;
-  },
-  setWeatherForecast : function(name, data){
-    this.name = name;
-    this.temperature = Math.floor(data.main.temp - 273);
-    this.humidity = data.main.humidity;
-    this.description = data.weather[0].description;
-  },
-  displayWeather : function(n){
-    let content;
-    if(n==0){
-      content = "Today It's " + this.description + " in "+ this.name + " at "+ this.temperature + "°C "+ "and "+ this.humidity+"% Humidity" ;
-    }else{
-     content = "Day "+n+" :- It will be " + this.description + " in "+ this.name + " at "+ this.temperature + "°C "+ "and "+ this.humidity+"% Humidity" ;
-    }
-    let div = document.createElement("div");
-    console.log(content);
-    div.innerHTML = content;
-    let container = document.querySelector('#weather');
-    container.appendChild(div);
-  },
-};
+    let weather = {
+    name : null,
+    temperature : null,
+    humidity : null,
+    description : null,
+    pressure : null,
+    wind : null,
+    setWeather : function(data){
+        weather.name = data.name;
+        weather.temperature = Math.floor(data.main.temp - 273.15);
+        weather.humidity = data.main.humidity;
+        weather.description = "";
+        for( let i = 0; i < data.weather.length; i++){
+        if(i == 0) weather.description += data.weather[i].description + " ";
+        else weather.description += "with " + data.weather[i].description + " ";
+        }
+        weather.pressure = data.main.pressure;
+        this.wind = data.wind.speed;
+    },
+    setWeatherForecast : function(name, data){
+        console.log(data);
+        this.name = name;
+        this.temperature = Math.floor(data.main.temp - 273);
+        this.humidity = data.main.humidity;
+        this.description = data.weather[0].description;
+        this.pressure = data.main.pressure;
+        this.wind = data.wind.speed;
+    },
+    displayWeather : function(n){
+        let content;
+        if(n==0){
+        content = "Today It's " + this.description + " in "+ this.name + " at "+ this.temperature + "°C "+ "and "+ this.humidity+"% Humidity, "+this.wind+"m/s wind speed and "+this.pressure+"mb Pressure" ;
+        }else{
+        content = "Day "+n+" :- It will be " + this.description + " in "+ this.name + " at "+ this.temperature + "°C "+ "and "+ this.humidity+"% Humidity, "+this.wind+"m/s wind speed and "+this.pressure+"mb Pressure" ;
+        }
+        let div = document.createElement("div");
+        console.log(content);
+        div.innerHTML = content;
+        let container = document.querySelector('#weather');
+        container.appendChild(div);
+    },
+    };
 
-function displayWeatherForecast(data){
-  let name = data.city.name;
-  for( let i = 0, j = 0; i < 40 ; i+=8, j++){
-    weather.setWeatherForecast(name, data.list[i]);
-  }
-}
-
-function getWeather( latitude, longitude){
-  let api = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${key}`;
-  
-  fetch(api)
-    .then(function(response){
-      let data = response.json();
-      return data;
-    })
-  .then(function(data){
-    console.log(data);
-    weather.setWeather(data);
-  })
-  .then(function(){
-    weather.displayWeather(0);
-  })
-}
-
-function getWeatherForecast( latitude, longitude){
-  let api = `https://api.openweathermap.org/data/2.5/forecast?lat=${latitude}&lon=${longitude}&appid=${key}`;
-  
-  fetch(api)
-    .then(function(response){
-    let data = response.json();
-    return data;
-  })
-  .then(function(data){
-    console.log(data);
+    function displayWeatherForecast(data){
     let name = data.city.name;
     for( let i = 0, j = 0; i < 40 ; i+=8, j++){
-      weather.setWeatherForecast(name, data.list[i]);
-      weather.displayWeather(j+1);
+        weather.setWeatherForecast(name, data.list[i]);
     }
-  })
+    }
+
+    function getWeather( latitude, longitude){
+    let api = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${key}`;
+    
+    fetch(api)
+        .then(function(response){
+        let data = response.json();
+        return data;
+        })
+    .then(function(data){
+        console.log(data);
+        weather.setWeather(data);
+    })
+    .then(function(){
+        weather.displayWeather(0);
+    })
+    }
+
+    function getWeatherForecast( latitude, longitude){
+    let api = `https://api.openweathermap.org/data/2.5/forecast?lat=${latitude}&lon=${longitude}&appid=${key}`;
+    
+    fetch(api)
+        .then(function(response){
+        let data = response.json();
+        return data;
+    })
+    .then(function(data){
+        console.log(data);
+        let name = data.city.name;
+        for( let i = 0, j = 0; i < 40 ; i+=8, j++){
+        weather.setWeatherForecast(name, data.list[i]);
+        weather.displayWeather(j+1);
+        }
+    })
+    }
+
+    function setPosition(position){
+    let latitude = position.coords.latitude;
+    let longitude = position.coords.longitude;
+    
+    getWeather(latitude, longitude);
+    getWeatherForecast(latitude, longitude);
+    }
+
+    if("geolocation" in navigator){
+    navigator.geolocation.getCurrentPosition(setPosition);
+    }
 }
 
-function setPosition(position){
-  let latitude = position.coords.latitude;
-  let longitude = position.coords.longitude;
-  
-  getWeather(latitude, longitude);
-  getWeatherForecast(latitude, longitude);
-}
 
-if("geolocation" in navigator){
-  navigator.geolocation.getCurrentPosition(setPosition);
-}
 
 
 
